10d40500c00d7afcc46e46ead8eec49056b8fc0e
Ana Luiza Ramalho <analuizaramalho9@gmail.com>
2020-07-29 01:16:04 +0000

    Criando CRUD Projects

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,14 +1,11 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
+const Project =use('App/Models/Project')
 /**
  * Resourceful controller for interacting with projects
  */
 class ProjectController {
-  /**
+  /*
    * Show a list of all projects.
    * GET projects
    *
@@ -18,56 +15,29 @@ class ProjectController {
    * @param {View} ctx.view
    */
   async index ({ request, response, view }) {
+      const projects=await Project.query().with('user').fetch()
+      return projects
+  }
+  async store ({ request, response,auth }) {
+
+    const data = request.only(['title','description'])
+    const project = await Project.create({ ...data,user_id:auth.user.id})
+
+    return project
   }
 
-  /**
-   * Render a form to be used for creating a new project.
-   * GET projects/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new project.
-   * POST projects
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
-
-  /**
-   * Display a single project.
-   * GET projects/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
   async show ({ params, request, response, view }) {
+
+      const project = await Project.findOrFail(params.id)
+      await project.load('user')
+      await project.load('tasks')
+
+      return project
+
   }
 
-  /**
-   * Render a form to update an existing project.
-   * GET projects/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
 
-  /**
+  /*
    * Update project details.
    * PUT or PATCH projects/:id
    *
@@ -76,17 +46,21 @@ class ProjectController {
    * @param {Response} ctx.response
    */
   async update ({ params, request, response }) {
+    const project = await Project.findOrFail(params.id)
+    const data = request.only(['title','description'])
+    project.merge(data)
+
+    await project.save()
+
+    return project
+
   }
 
-  /**
-   * Delete a project with id.
-   * DELETE projects/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+
   async destroy ({ params, request, response }) {
+    const project = await Project.findOrFail(params.id)
+
+    await project.delete()
   }
 }
 
